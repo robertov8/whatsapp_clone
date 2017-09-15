74a2cddd981b9e8a9b8175561c4ec7e2dd07ca87
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-15 20:03:38 -0300

    Section 15, class 205

--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, TextInput, Button } from 'react-native';
+import { connect } from 'react-redux';
 
+class FormCadastro extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, padding: 10 }}>
+                <View style={{ flex: 4, justifyContent: 'center' }}>
+                    <TextInput value={this.props.nome} style={{ fontSize: 20, height: 45 }} placeholder='Nome' />
+                    <TextInput value={this.props.email} style={{ fontSize: 20, height: 45 }} placeholder='E-mail' />
+                    <TextInput value={this.props.senha} style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
+                </View>
+                <View style={{ flex: 1 }}>
+                    <Button title='Cadastrar' color='#115E54' onPress={() => false } />
+                </View>
+            </View>
+        );
+    }
+}
 
-export default props => (
-    <View style={{ flex: 1, padding: 10 }}>
-        <View style={{ flex: 4, justifyContent: 'center' }}>
-            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='Nome' />
-            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='E-mail' />
-            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
-        </View>
-        <View style={{ flex: 1 }}>
-            <Button title='Cadastrar' color='#115E54' onPress={() => false } />
-        </View>
-    </View>
-);
+const mapStateToProps = state => ({
+    nome: state.AutenticaoReducer.nome,
+    email: state.AutenticaoReducer.email,
+    senha: state.AutenticaoReducer.senha
+});
+
+export default connect(mapStateToProps, null)(FormCadastro);
