25bf4290dab5dd742a70380b69acfdd734dd1c05
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-15 18:54:57 -0300

    Section 15, class 198

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 
 export default props => (
-    <View>
-        <View>
-            <Text>WhatsApp Clone</Text>
+    <View style={{ flex: 1, padding: 10 }}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 25 }}>WhatsApp Clone</Text>
         </View>
 
-        <View>
-            <TextInput placeholder='E-mail' />
-            <TextInput placeholder='Senha' />
+        <View style={{ flex: 2 }}>
+            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='E-mail' />
+            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
+            <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
         </View>
 
-        <View>
-            <Button title='Acessar' onPress={() => false}/>
+        <View style={{ flex: 2 }}>
+            <Button title='Acessar' color='#115E54' onPress={() => false}/>
         </View>
     </View>
 )
