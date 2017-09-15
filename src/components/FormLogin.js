0506454eea9c8130db45e72ed4ce807b6375e9a1
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-15 19:53:03 -0300

    Section 15, class 204

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, TextInput, Button, TouchableHighlight } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { connect } from 'react-redux';
 
-export default props => (
-    <View style={{ flex: 1, padding: 10 }}>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 25 }}>WhatsApp Clone</Text>
-        </View>
+class FormLogin extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, padding: 10 }}>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text style={{ fontSize: 25 }}>WhatsApp Clone</Text>
+                </View>
 
-        <View style={{ flex: 2 }}>
-            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='E-mail' />
-            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
-            <TouchableHighlight onPress={() => Actions.formCadastro()}>
-                <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
-            </TouchableHighlight>
-        </View>
+                <View style={{ flex: 2 }}>
+                    <TextInput value={this.props.email} style={{ fontSize: 20, height: 45 }} placeholder='E-mail' />
+                    <TextInput value={this.props.senha} style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
+                    <TouchableHighlight onPress={() => Actions.formCadastro()}>
+                        <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
+                    </TouchableHighlight>
+                </View>
 
-        <View style={{ flex: 2 }}>
-            <Button title='Acessar' color='#115E54' onPress={() => false}/>
-        </View>
-    </View>
-);
+                <View style={{ flex: 2 }}>
+                    <Button title='Acessar' color='#115E54' onPress={() => false}/>
+                </View>
+            </View>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    email: state.AutenticaoReducer.email,
+    senha: state.AutenticaoReducer.senha
+});
+
+export default connect(mapStateToProps, null)(FormLogin);
