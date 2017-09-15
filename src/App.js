39411c8c761403a881cc9aa244e6a728b14598f3
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-15 19:29:39 -0300

    Section 15, class 201

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Routes from './Routes';
 
 export default props => (
-    <Routes />
+    <Provider store={createStore(reducers)}>
+        <Routes />
+    </Provider>
 );
