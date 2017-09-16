2942e4d525af387cde0cb3f25656864dae190215
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-15 21:38:12 -0300

    Section 15, class 217

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers';
+import { config } from './firebase.js';
 
 const store = createStore(reducers);
 
-export default props => (
-    <Provider store={store}>
-        <Routes />
-    </Provider>
-);
+class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp(config);
+    }
+    render() {
+        return (
+            <Provider store={store}>
+                <Routes />
+            </Provider>
+        );
+    }
+}
+
+export default App;
