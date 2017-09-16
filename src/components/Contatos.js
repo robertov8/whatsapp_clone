7106f9ea59a4699b898748bddada02f53e9f58d8
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-16 19:30:37 -0300

    Section 15, class 264

--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import { contatosUsuarioFetch } from '../actions/AppActions';
 
@@ -19,4 +20,12 @@ class Contatos extends Component {
     }
 }
 
-export default connect(null, { contatosUsuarioFetch })(Contatos);
+const mapStateToProps = state => {
+    const contatos = _.map(state.ListaContatosReducer, (val, uid) => {
+        return { ...val, uid };
+    });
+    console.log(contatos);
+    return {}
+};
+
+export default connect(mapStateToProps, { contatosUsuarioFetch })(Contatos);
--- src/reducers/ListaContatosReducer.js
+++ src/reducers/ListaContatosReducer.js
@@ -1,8 +1,11 @@
+import { LISTA_CONTATO_USUARIO } from '../actions/types';
+
 const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
-    console.log('teste', action.payload);
     switch(action.type) {
+        case LISTA_CONTATO_USUARIO:
+            return action.payload;
         default:
             return state;
     }
