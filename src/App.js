2357c53d99b809cb1a4c78984ce8fc54baf89da7
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-15 22:30:21 -0300

    Section 15, class 221

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThubk from 'redux-thunk';
 import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers';
 import { config } from './firebase.js';
 
-const store = createStore(reducers);
+const store = createStore(reducers, {}, applyMiddleware(ReduxThubk));
 
 class App extends Component {
 
     componentWillMount() {
         firebase.initializeApp(config);
     }
+    
     render() {
         return (
             <Provider store={store}>
