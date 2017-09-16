2f45c3aadb455ff9fa1b54a9b840464dc9be9866
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-16 02:41:46 -0300

    Section 15, class 226

--- src/actions/AutenticacaoActions.js
+++ src/actions/AutenticacaoActions.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
+import b64 from 'base-64';
 
 export const modificaNome = (texto) => {
     return {
@@ -25,7 +26,14 @@ export const modificaSenha = (texto) => {
 export const cadastraUsuario = ({ nome, email, senha }) => {
     return dispatch => {
         firebase.auth().createUserWithEmailAndPassword(email, senha)
-        .then(user => cadastroUsuarioSucesso(dispatch))
+        .then(user => {
+            let emailB64 = b64.encode(email);
+
+            firebase.database()
+                .ref(`/contatos/${emailB64}`)
+                .push({ nome })
+                .then(value => cadastroUsuarioSucesso(dispatch));
+        })
         .catch(erro => cadastroUsuarioErro(erro, dispatch));
     };
 };
