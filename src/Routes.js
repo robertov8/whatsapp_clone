de5fa1c0bb609c0e52fd8fb1570b4e0fad7ff69c
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-16 15:13:32 -0300

    Section 15, class 243

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import FormLogin from './components/FormLogin';
 import FormCadastro from './components/FormCadastro';
 import BoasVindas from './components/BoasVindas';
 import Principal from './components/Principal';
+import adicionarContato from './components/AdicionarContato';
 
 export default props => (
     <Router
@@ -16,6 +17,7 @@ export default props => (
             <Scene key='formCadastro' component={FormCadastro} title='Cadastro' hideNavBar={false} />
             <Scene key='boasVindas' component={BoasVindas} title='Bem-Vindo' hideNavBar={true} />
             <Scene key='principal' component={Principal} title='Principal' hideNavBar={true} initial />
+            <Scene key='adicionarContato' component={adicionarContato} title='Adicionar Contato' hideNavBar={false} />
         </Stack>
     </Router>
 );
--- src/components/TabBarMenu.js
+++ src/components/TabBarMenu.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { View, Text, StatusBar, Image, TouchableHighlight } from 'react-native';
 import { TabBar } from 'react-native-tab-view';
+import { Actions } from 'react-native-router-flux';
+
+const iconAdicionarContato = require('../imgs/adicionar-contato.png');
 
 export default props => (
     <View style={{ backgroundColor: '#115E54', elevation: 4 }}>
         <StatusBar backgroundColor='#114D44' />
 
-        <View style={{ height: 50, justifyContent: 'center' }}>
-            <Text style={{ color: '#fff', fontSize: 20, marginLeft: 20 }}>WhatsApp Clone</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <View style={{ height: 50, justifyContent: 'center' }}>
+                <Text style={{ color: '#fff', fontSize: 20, marginLeft: 20 }}>WhatsApp Clone</Text>
+            </View>
+            
+            <View style={{ flexDirection: 'row', marginRight: 20 }}>
+                <View style={{ width: 50, justifyContent: 'center', alignItems: 'center' }}>
+                    <TouchableHighlight
+                        onPress={() => Actions.adicionarContato()}
+                        underlayColor='#114D44'
+                    >
+                        <Image source={iconAdicionarContato} />
+                    </TouchableHighlight>
+                </View>
+
+                <View style={{ justifyContent: 'center' }}>
+                    <Text style={{ fontSize: 20, color: '#fff' }}>Sair</Text>
+                </View>
+            </View>
         </View>
 
         <TabBar {...props} style={{ backgroundColor: '#115E54', elevation: 0 }} />
