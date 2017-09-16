192c6ea5b6e3d3c30fae474583062072d34e8a38
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-16 20:02:28 -0300

    Section 15, class 265

--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
 import { contatosUsuarioFetch } from '../actions/AppActions';
 
 class Contatos extends Component {
+    constructor(props) {
+        super(props);
+
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.state = { fonteDeDados: ds.cloneWithRows([
+            'Registro 1',
+            'Registro 2',
+            'Registro 3',
+            'Registro 4'
+        ])};
+    }
 
     componentWillMount() {
         this.props.contatosUsuarioFetch();
@@ -13,9 +27,10 @@ class Contatos extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Contatos</Text>
-            </View>
+            <ListView
+                dataSource={this.state.fonteDeDados}
+                renderRow={data => <View><Text>{data}</Text></View>}
+            />
         );
     }
 }
