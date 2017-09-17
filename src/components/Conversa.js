e4f0c47dac7f500d13676734e837f8a12178edfc
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-17 01:37:59 -0300

    Section 15, class 271

--- src/components/Conversa.js
+++ src/components/Conversa.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableHighlight, Image } from 'react-native';
+
+const btnEnviar = require('../imgs/enviar_mensagem.png');
 
 export default class Conversa extends Component {
     render() {
         return (
-            <View>
-                <Text>Conversa</Text>
+            <View style={{ flex: 1, backgroundColor: '#eee4dc', padding: 10 }}>
+                <View style={{ flex: 1, paddingBottom: 20 }}></View>
+
+                <View style={{ flexDirection: 'row', height: 60 }}>
+                    <TextInput
+                        style={{ flex: 4, backgroundColor: '#fff', fontSize: 18 }}
+                    />
+
+                    <TouchableHighlight
+                        onPress={() => false}
+                        underlayColor='#fff'
+                    >
+                        <Image source={btnEnviar} />
+                    </TouchableHighlight>
+                </View>
             </View>
         );
     }
