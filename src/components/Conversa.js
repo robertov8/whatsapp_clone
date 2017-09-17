df08c668caf318cc30a713cbbb7421da782b0c2d
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-17 15:43:10 -0300

    Section 15, class 280

--- src/components/Conversa.js
+++ src/components/Conversa.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableHighlight, Image } from 'react-native';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableHighlight,
+    Image,
+    ListView
+} from 'react-native';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
@@ -11,7 +18,18 @@ class Conversa extends Component {
 
     componentWillMount() {
         this.props.conversaUsuarioFetch(this.props.contatoEmail);
+        this.criaFonteDeDados(this.props.conversa);
     }
+
+    componentWillReceiveProps(nextProps) {
+        this.criaFonteDeDados(nextProps.conversa);
+    }
+
+    criaFonteDeDados(conversa) {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.dataSource = ds.cloneWithRows(conversa);
+    }
+
 
     _enviarMensagem() {
         const { mensagem, contatoNome, contatoEmail } = this.props;
@@ -19,10 +37,25 @@ class Conversa extends Component {
         this.props.enviarMensagem(mensagem, contatoNome, contatoEmail);
     }
 
+    renderRow(texto) {
+        return (
+            <View>
+                <Text>{texto.mensagem}</Text>
+                <Text>{texto.tipo}</Text>
+            </View>
+        );
+    }
+
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: '#eee4dc', padding: 10 }}>
-                <View style={{ flex: 1, paddingBottom: 20 }}></View>
+                <View style={{ flex: 1, paddingBottom: 20 }}>
+                    <ListView
+                        enableEmptySections
+                        dataSource={this.dataSource}
+                        renderRow={this.renderRow}
+                    />
+                </View>
 
                 <View style={{ flexDirection: 'row', height: 60 }}>
                     <TextInput
@@ -48,8 +81,6 @@ const mapStateToProps = state => {
     const conversa = _.map(state.ListaConversaReducer, (val, uid) => {
         return { ...val, uid };
     });
-
-    console.log(conversa);
 
     return ({
         conversa,
