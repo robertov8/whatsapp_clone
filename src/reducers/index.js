09270fbcecbdcc12b28cf87dc13039114733e13a
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-17 13:19:24 -0300

    Section 15, class 277

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import AutenticaoReducer from './AutenticaoReducer';
 import AppReducer from './AppReducer';
 import ListaContatosReducer from './ListaContatosReducer'
+import ListaConversaReducer from './ListaConversaReducer';
 
 export default combineReducers({
     AutenticaoReducer,
     AppReducer,
+    ListaContatosReducer,
     ListaContatosReducer
 });
